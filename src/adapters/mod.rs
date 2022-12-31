65cb3db63edb16f865f4277d021f853d755b3faf
yswtrue <a365039311@gmail.com>
2023-01-01 02:20:15 +0800

    http status check

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -9,10 +9,19 @@ pub trait Scraper {
     fn registry_url(&self) -> &str;
     fn dis_match_text(&self) -> &str;
     async fn check(&self) -> Result<bool> {
-        let resp = reqwest::get(self.registry_url()).await?.text().await?;
-        if !resp.contains(self.dis_match_text()) {
-            utils::send_email(format!("{}已开放注册", self.name()), resp).await?;
+        let resp = reqwest::get(self.registry_url()).await?;
+        let resp_status = resp.status();
+        let resp_text = resp.text().await?;
+        if resp_status.is_success() && !resp_text.contains(self.dis_match_text()) {
+            utils::send_email(format!("{}已开放注册", self.name()), resp_text).await?;
             return Ok(true);
+        }
+        if !resp_status.is_success() {
+            log::debug!(
+                "request {} error: {}",
+                self.registry_url(),
+                resp_status.to_string()
+            )
         }
         Ok(false)
     }
